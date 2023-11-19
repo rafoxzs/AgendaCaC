d0bd7cd66545e5b1c7fd82c6a3b190b57d63d037
rafoxzs <rafdangs@gmail.com>
2023-11-19 02:17:55 +0100

    comprobaciones ok

--- Tps finales/Agenda tp2/Agendav1.py
+++ Tps finales/Agenda tp2/Agendav1.py
@@ -37,6 +37,33 @@ def cons_contacto(nombre):
         print("El contacto no existe en la agenda.")
 
 
+def verif_name():
+    while True:
+        nombre = input("ingrese el nombre :")
+        if nombre.replace(" ", "").isalpha() or nombre.replace("-", "").isalpha() or nombre.replace("'", "").isalpha():
+            return str(nombre)
+        else:
+            print("Por favor, ingrese un nombre y apellido valido.")
+
+
+def verif_tel():
+    while True:
+        telefono = input("ingrese el telefono :")
+        if len(str(telefono)) > 6 and telefono.isdigit():
+            return int(telefono)
+        else:
+            print("Por favor, ingrese un telefono valido.")
+
+
+def verif_email():
+    while True:
+        email = input("ingrese el email :")
+        if "@" in email and "." in email[email.index("@"):]:
+            return str(email)
+        else:
+            print("Por favor, ingrese un email valido.")
+
+
 def main():
     while True:
         print("\n1. Agregar contacto")
@@ -47,30 +74,25 @@ def main():
         opcion = int(input("Selecciona una opción: "))
         match opcion:
             case 1:
-                # aca hay que meter una comprobacion que sean todas letras
-                nombre = input("ingrese el nombre :")
-                # aca hay que meter una comprobacion que sean todos numeros
-                telefono = input("ingrese el telefono :")
-                # hay que poner una comprobacion que detecte el arroba y el .com
-                email = input("ingrese el Email :")
-                # nidea que comprobacion meterle
+                nombre = verif_name()
+                telefono = verif_tel()
+                email = verif_email()
                 direccion = input("ingrese direccion :")
                 add_contacto(nombre, telefono, email, direccion)
             case 2:
-                # aca hay que meter una comprobacion que sean todas letras
-                nombre = input("nombre del contacto a modificar :")
-                # aca hay que meter una comprobacion que sean todos numeros
-                telefono = input("telefono nuevo :")
-                # hay que poner una comprobacion que detecte el arroba y el .com
-                email = input("email nuevo :")
-                # nidea que comprobacion meterle
+                print("Modificar contacto")
+                nombre = verif_name()
+                telefono = verif_tel()
+                email = verif_email()
                 direccion = input("nueva direccion :")
                 mod_contacto(nombre, telefono, email, direccion)
             case 3:
-                nombre = input("nombre del contacto a eliminar :")
+                print("Eliminar contacto")
+                nombre = verif_name()
                 elim_contacto(nombre)
             case 4:
-                nombre = input("nombre del contacto a consultar :")
+                print("Consultar contacto")
+                nombre = verif_name()
                 cons_contacto(nombre)
             case 5:
                 print("Hasta luegito!")
@@ -84,8 +106,8 @@ if __name__ == "__main__":
 
 
 # to do list
-#    - usbirlo a github y empezar a usar control de versiones
-#    - implementar funciones de comprobacion
+#    - usbirlo a github y empezar a usar control de versionesa  ok
+#    - implementar funciones de comprobacion ok FALTARIA VER COMO HACER PARA QUE DEPENDIENDO SEA EL CASO , AGREGAR O MODIFICAR CAMBIE EL PRINT DE CADA F
 #    - funcion que me muestre todos los contactos agendados
 #    - guardar en archivo
 #    -guardarlos con base de datos
@@ -93,4 +115,12 @@ if __name__ == "__main__":
 #
 #
 #
-#
+# #Verificar cambios: Antes de hacer un commit, verifica los cambios que se van a incluir con git status. Esto te mostrará qué archivos han sido modificados, agregados o eliminados.
+
+# Agregar archivos: Usa git add <nombre_archivo> para agregar archivos específicos o git add . para incluir todos los archivos modificados en el commit.
+
+# Realizar el commit: Ejecuta git commit -m "Mensaje descriptivo aquí" para confirmar los cambios. Es importante incluir un mensaje claro que describa los cambios realizados.
+
+# Verificar el estado: Después del commit, puedes usar nuevamente git status para asegurarte de que no haya más cambios pendientes por agregar.
+
+# Recuerda que estos pasos te permiten guardar tus cambios de forma local. Para enviar los cambios al repositorio remoto, se usa git push.
