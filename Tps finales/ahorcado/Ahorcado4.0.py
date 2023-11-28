6d0d6290110993cd98114f11e5ea89168bcc2f5d
rafoxzs <rafdangs@gmail.com>
2023-11-28 03:28:41 +0100

    version 4.5 con acumulacion de puntos , no repite palabras

--- Tps finales/ahorcado/Ahorcado4.0.py
+++ Tps finales/ahorcado/Ahorcado4.0.py
@@ -4,23 +4,34 @@ import random
 # Cada uno le pone la ruta en la cual lo tiene TIP: copiar ruta de acceso relativa y le cambian el sentido de las barras
 ruta = "Tps finales/ahorcado/"
 
+palabras_usadas = []
+
 
 def palabras_faciles():  # Se crea una funcion para cada dificultad del juego.
     palabras = ['perro', 'gato', 'mesa', 'silla', 'variable', 'imprimir',
                 'lista', 'suma', 'computadora', 'programa', 'pajaro', 'pelota']
-    return random.choice(palabras)
+    while True:
+        palabra = random.choice(palabras)
+        if palabra not in palabras_usadas:
+            return palabra
 
 
 def palabras_intermedias():
     palabras = ['elefante', 'leopardo', 'heladera', 'ventilador', 'condicion',
                 'diccionario', 'bucle', 'funcion', 'algoritmo', 'iteracion', 'cocodrilo', 'teclado']
-    return random.choice(palabras)
+    while True:
+        palabra = random.choice(palabras)
+        if palabra not in palabras_usadas:
+            return palabra
 
 
 def palabras_dificiles():
-    palabras = ['quimera', 'ornitórrinco', 'microscopio', 'espectroscopio', 'polimorfismo', 'abstraccion',
+    palabras = ['quimera', 'ornitorrinco', 'microscopio', 'espectroscopio', 'polimorfismo', 'abstraccion',
                 'herencia', 'recursividad', 'electrico', 'monosacarido', 'anfibio', 'biodegradabilidad']
-    return random.choice(palabras)
+    while True:
+        palabra = random.choice(palabras)
+        if palabra not in palabras_usadas:
+            return palabra
 
 
 # Definimos una funcion para mostrar las casillas a llenar.
@@ -35,32 +46,31 @@ def mostrar_tablero(palabra, letras_adivinadas):
     return resultado.strip()
 
 
+def get_palabra(dificultad):
+    if dificultad == 1:
+        palabra_secreta = palabras_faciles()
+        return palabra_secreta
+    if dificultad == 2:
+        palabra_secreta = palabras_intermedias()
+        return palabra_secreta
+    if dificultad == 3:
+        palabra_secreta = palabras_dificiles()
+        return palabra_secreta
+
+
 def select_dif():
     print("Selecciona la dificultad: ")
     print("1. Facil")
     print("2. Normal")
     print("3. Dificíl")
-
-    while True:
-        dificultad = int(input(""))
-        match dificultad:
-            case 1:
-                palabra_secreta = palabras_faciles()
-                return palabra_secreta
-            case 2:
-                palabra_secreta = palabras_intermedias()
-                return palabra_secreta
-            case 3:
-                palabra_secreta = palabras_dificiles()
-                return palabra_secreta
+    dificultad = int(input(""))
+    return dificultad
 
 
-def jugar(palabra_secreta):
-    # esto hay que pasarselo todo desde la otra funcion
-    palabra_secreta = palabras_faciles()
+def jugar(palabra_secreta, puntos):
+
     intentos_restantes = 6
     letras_adivinadas = []
-    puntos = 0
 
     palabra_oculta = "_" * len(palabra_secreta)
 
@@ -89,14 +99,16 @@ def jugar(palabra_secreta):
 
         if "_" not in palabra_oculta:
             print("¡Felicidades! Descubriste la palabra!")
+            perdiste = False
             break
-
         if intentos_restantes == 0:
             print("\n O")
             print("/|\\")
             print("/ \\")
             print("\n¡Oh no! Te quedaste sin intentos. La palabra era:",
                   palabra_secreta)
+            perdiste = True
+
         if intentos_restantes == 5:
             print("\nO")
         elif intentos_restantes == 4:
@@ -113,10 +125,7 @@ def jugar(palabra_secreta):
             print("\n O")
             print("/|\\")
             print("/")
-
-    nombre_jugador = input("Ingresa tu nombre para registrar tu puntaje: ")
-    with open(ruta + "Puntaje.txt", "a") as puntaje:
-        puntaje.write(f"{nombre_jugador}: {puntos}\n")
+    return perdiste, puntos
 
 
 def show_creditos():
@@ -157,13 +166,19 @@ def show_puntajes():
     input("Presiona Enter para continuar...")
 
 
+def grabar_puntaje(puntos):
+    nombre_jugador = input("Ingresa tu nombre para registrar tu puntaje: ")
+    with open(ruta + "Puntaje.txt", "a") as puntaje:
+        puntaje.write(f"{nombre_jugador}, {puntos}\n")
+
+
 def main():
     print("Bienvenido al juego del ahorcado.\n")
     print("Las reglas son simples. La computadora elige una palabra al azar y debes adivinar la palabra correcta indicando una letra a la vez. ")
     print("\nCada acierto te muestra la letra que adivinaste en el lugar que tiene en la palabra.")
     print("\nTienes 6 intentos para adivinar. Cada acierto suma un punto!\n")
     while True:
-
+        puntos = 0
         print("1. Ver puntajes.")
         print("2. Jugar.")
         print("3 Creditos")
@@ -173,8 +188,25 @@ def main():
             case 1:  # muestra los puntajes
                 show_puntajes()
             case 2:
-                palabra_secreta = select_dif()
-                jugar(palabra_secreta)
+                dificultad = select_dif()
+                perdiste = False
+                jugar_de_nuevo = True
+                while jugar_de_nuevo:
+                    palabra_secreta = get_palabra(dificultad)
+                    print(palabra_secreta)  # esta solo con motivos de TESTING
+                    palabras_usadas.append(palabra_secreta)
+                    perdiste, puntos = jugar(palabra_secreta, puntos)
+                    if perdiste == True:
+                        print(f"puntos acumulados : {puntos}")
+                        grabar_puntaje(puntos)
+                        break
+                    print(f"puntos acumulados : {puntos}")
+                    seguir_jugando = input(
+                        "Quieres seguir jugando? s/n\n").lower()
+                    if seguir_jugando == "n":
+                        grabar_puntaje(puntos)
+                        jugar_de_nuevo = False
+
             case 3:
                 show_creditos()
             case 4:
@@ -194,9 +226,12 @@ if __name__ == "__main__":
 # - los input ya los tomo como INT , asi te ahorras todo el codigo que chekea que sea int , y sino lo convierte
 # - Agregue la funcion show_creditos , que muestra los creditos , podriamos hacer uns lista con los miembros del grupo , para no complicarla leyendo un archivo
 # - Agregue la funcion show_puntajes , agarra el txt , separa los valores , los ordena de mayor a menor y los muestra en pantalla
-#
+
 #
 # COSAS Q faltarian hacer
 
 # - que se pueda seguir jugando cuando ganas
 # - que el puntaje lo pida una ves que perdes
+
+
+# - LO subi a github les dejo un tuto  https://www.youtube.com/watch?v=eQMcIGVc8N0&t=203s
